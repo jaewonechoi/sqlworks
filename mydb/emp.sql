50d16402dd3c5671fa2a44da5c06626ea076fc52
jaewonechoi <mybity2@gmail.com>
2023-10-16 15:21:00 +0900

    subquery and view

--- mydb/emp.sql
+++ mydb/emp.sql
@@ -13,8 +13,6 @@ INSERT INTO emp VALUES (101, '김산', '여자', 2500000, '2020-05-15');
 INSERT INTO emp VALUES (102, '오상식', '남자', 5000000, '2015-02-22');
 INSERT INTO emp VALUES (103, '박신입', '여자', null, '2023-10-01');
 
-SELECT * FROM emp;
-
 -- 실습 문제 풀이
 -- 1. 사원을 입사일 순으로 정렬하시오(오름차순 정렬)
 SELECT * FROM emp
@@ -31,13 +29,13 @@ ORDER BY salary DESC;
 
 -- 4. 급여가 없는 사원을 검색하시오
 SELECT * FROM emp
-where salary is null;
+WHERE salary IS NULL;
 
 -- 5. 사원의 총 수를 구하시오
-SELECT COUNT(*) 사원총수 FROM emp;
+SELECT COUNT(*) 총사원수 FROM emp;
 
 -- 6. 사원의 급여 합계와 평균을 구하시오
-SELECT SUM(salary) 급여합계, AVG(salary) 급여평균 FROM emp;
+SELECT SUM(salary), AVG(salary) FROM emp;
 
 -- 7. 이름이 김산인 사원의 성별을 남자로 변경하시오
 UPDATE emp SET gender = '남자'
@@ -47,8 +45,6 @@ WHERE ename = '김산';
 DELETE FROM emp
 WHERE ename = '오상식';
 
-COMMIT;
-
 -- 반올림 : ROUND(숫자, 자릿수)
 SELECT ename 사원이름,
        salary 급여,
@@ -57,3 +53,19 @@ SELECT ename 사원이름,
        ROUND(salary/30, 0) 결과2,
        ROUND(salary/30, -1) 결과3
 FROM emp;
+
+-- 급여가 가장 많은 사원을 검색하시오
+-- MAX(salary)
+-- 서브쿼리(subquery), 중첩쿼리(메인쿼리, 서브쿼리)
+-- 최고급여, 최저급여 구하기
+SELECT MAX(salary) FROM emp;
+SELECT MIN(salary) FROM emp;
+
+SELECT ename, salary
+FROM emp
+WHERE salary = (SELECT MAX(salary) FROM emp) 
+   or salary = (SELECT MIN(salary) FROM emp);
+
+SELECT * FROM emp;
+
+COMMIT;
--- mydb/join.sql
+++ mydb/join.sql
@@ -2,6 +2,10 @@
 -- 동등조인(equi join) : 조인 조건이 정확히 일치하는 경우에 결과 출력
 -- 외부조인(outer join) : 조인 조건이 정확히 일치하지 않아도 모든 결과를 출력
 
+SELECT * FROM book;
+SELECT * FROM customer;
+SELECT * FROM orders;
+
 -- 고객(customer)과 고객의 주문(orders)에 관한 데이터를 모두 검색하시오
 -- 고객별 주문 금액의 총액을 출력하시오(Group by 결과)
 -- '김연아' 고객의 주문 총금액을 출력(HAVING으로 제한을 둠)
@@ -30,7 +34,7 @@ WHERE cus.custid = ord.custid   -- 기본키 = 외래키
 -- 고객(customer)과 고객의 주문(orders)에 관한 데이터 중 고객의 이름과
 -- 고객이 주문한 도서의 판매가격을 검색하시오
 SELECT cus.name, ord.saleprice 
-FROM customer cus INNER JOIN orders ord
+FROM customer cus INNER JOIN orders ord     --INNER 생략가능
   ON cus.custid = ord.custid
 ORDER BY cus.name;
 
@@ -43,6 +47,14 @@ ORDER BY cus.name;
 SELECT cus.name, ord.saleprice
 FROM customer cus LEFT OUTER JOIN orders ord
   ON cus.custid = ord.custid
+ORDER BY cus.name;
+
+-- 고객(customer)
+-- 고객이름으로 정렬 (자동으로 정렬화 됨)
+-- 카테시안곱 : customer(6) x orders(10) = 60
+SELECT cus.name
+FROM customer cus, orders ord
+WHERE cus.custid = ord.custid
 ORDER BY cus.name;
 
 COMMIT;
